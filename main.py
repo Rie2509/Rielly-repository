0ee758a63f306ce14a41d301d45553b823c424c7
Rie2509 <159850198+Rie2509@users.noreply.github.com>
2024-05-03 12:24:03 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#Ask the user their name and save it
+input ("what is your name")
+#Greet the user and introduce the quiz
+print ("welcome to this quiz about valorant")
+#Ask the user a question
+input ("what is the most expensive weapon in valorant")
+print ("the operator")
+#end the quiz
+print ("the end")
