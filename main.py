a0398906f67ffe3234a354a646cb7cc65b68990c
Rie2509 <159850198+Rie2509@users.noreply.github.com>
2024-05-07 12:24:15 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+score = 0
+
+
 #Ask the user their name and save it
 name = input ("what is your name") 
 #Greet the user and introduce the quiz
@@ -5,28 +8,43 @@ print ("welcome to this quiz about valorant\n",name)
 #Ask the user a question 
 answer = input ("what is the most expensive weapon in valorant\n")
 if answer == "operator":
-    print ("correct")
+    print ("correct!")
+    score += 5
+elif answer == "":
+    print ("you didint answer")
 else:
     print ("incorrect")
 answer = input ("how many sentinals are in valorant\n")
 if answer == "5":
-    print ("correct")
+    print ("correct!")
+    score += 5
+elif answer == "":
+    print ("you didint answer")
 else:
     print ("incorrect")
 answer = input ("how many rounds to you need to win an unrated valorant match\n")
 if answer == "13":
-    print ("correct")
+    print ("correct!")
+    score += 5
+elif answer == "":
+    print ("you didint answer")
 else:
     print ("incorrect")
 answer = input ("which agent uses a dash ability\n")
 if answer == "jett":
-    print ("correct")
+    print ("correct!")
+    score += 5
+elif answer == "":
+    print ("you didint answer")
 else:
     print ("incorrect")
 answer = input ("which agent burnt down a school building\n")
 if answer == "phoenix":
-    print ("correct")
+    print ("correct!")
+    score += 5
+elif answer == "":
+    print ("you didint answer")
 else:
     print ("incorrect")
 #end the quiz
-print ("the end")
+print ("the end",score,"points")
